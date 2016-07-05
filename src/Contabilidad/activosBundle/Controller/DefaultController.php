8b1d6dc8bdeef7d26d9eb04cb9cab40fecfd1efc
Frank J Vigil Vega <frank.vigil@upr.edu.cu>
2016-07-05 12:11:52 -0400

    completando el modulo de activo fijo

--- src/Contabilidad/activosBundle/Controller/DefaultController.php
+++ src/Contabilidad/activosBundle/Controller/DefaultController.php
@@ -11,8 +11,58 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+
+   
+
     public function homeAction(){
          return $this->render('activosBundle:Default:home.html.twig');
+    }
+
+    public function activos_cnmbAction()
+    {
+         $peticion = $this->getRequest();//objeto POST or GET de una peticion URL
+        $idcnmb=$peticion->get('idcnmb');//obtener el Objeto "id" pasado por la peticion          
+
+        $idcnmb=$this->GetID($idcnmb);//separar el objeto id para obtener los numeros reales del id
+
+        $id=$peticion->get('id');//obtener el Objeto "id" pasado por la peticion        
+        $id=$this->GetID($id);//separar el objeto id para obtener los numeros reales del id
+        
+        $client = new Client("http://apiassets.upr.edu.cu");//abrir un nuevo cliente guzzle
+        $json=Array();//crear una variable json de tipo array
+
+            $request = $client->get("activo_fijos?idCcosto=".(string)$id."&cnmb=".(string)$idcnmb);//hacerle una peticion GET a la paguina consecutiva
+            $response = $request->send();//enviar la peticion
+            $data = $response->json(); //recoger el json de la peticion            
+            
+            $count=$data['hydra:lastPage'];;         
+            $count=$this->GetCantMultiFilter($count);                     
+
+            if ($count!=0) {
+                for ($i=1; $i <$count ; $i++) { 
+
+                    $request = $client->get("activo_fijos?idCcosto=".(string)$id."&cnmb=".(string)$idcnmb."&page=".(string)$i);
+                    $response = $request->send();            
+                    $data = $response->json(); 
+                    $src=$data['hydra:member'];   
+
+                    for ($j=0; $j < count($src) ; $j++) //recorrer los elementos del array que esta en "hydra:member"
+                        {             
+                            array_push($json, $src[$j]);//adicionarle al json los elementos del array que pertenesca al centro de costo                        
+                        }
+                }
+            }          
+            
+            
+        
+      
+      //renderizar al html los objetos json capturados
+        return $this->render('activosBundle:Default:activos.html.twig',array(
+            "json"=>$json,          
+            
+            ));
+        
+        
     }
 
     public function activosAction()//saber los activos fijos  dado el id de un area
@@ -28,10 +78,10 @@ class DefaultController extends Controller
             $request = $client->get("activo_fijos?idCcosto=".(string)$id);//hacerle una peticion GET a la paguina consecutiva
             $response = $request->send();//enviar la peticion
             $data = $response->json(); //recoger el json de la peticion
-            
-            $src=$data['hydra:member']; //separar el json de la respuesta
-            $count=$data['hydra:lastPage'];
-            $count=$this->GetCantPageFilter($count);          
+                 
+
+            $count=$data['hydra:lastPage'];         
+            $count=$this->GetCantPageFilter($count);  
 
             if ($count!=0) {
                 for ($i=1; $i <$count ; $i++) { 
@@ -60,7 +110,9 @@ class DefaultController extends Controller
     {
 
     	$count=$this->GetCantPage('centro_costos');
+        $countcnmb=$this->GetCantPage('cnmb_activofijos');
         $json=Array();
+        $jsoncnmb= Array();
         $client = new Client("http://apiassets.upr.edu.cu");
 
         if ($count!=0) {
@@ -87,10 +139,23 @@ class DefaultController extends Controller
                    }   
             
         }
+
+        for ($i=1; $i <$countcnmb ; $i++) {  
+              
+                $request = $client->get("cnmb_activofijos?page=".(string)$i);                    
+                $response = $request->send();            
+                $data = $response->json(); 
+                $src=$data['hydra:member'];                 
+                for ($j=0; $j < count($src) ; $j++) { 
+                           array_push($jsoncnmb, $src[$j]);
+                       } 
+            
+            }
     	
     	
         return $this->render('activosBundle:Default:index.html.twig',array(
         	"json"=>$json,
+            "jsoncnmb"=>$jsoncnmb,
             
         	));
     }
@@ -127,25 +192,28 @@ class DefaultController extends Controller
         }
     }
 
-    public function GetCantPageFilter($tabla)//obtener la cantidad de paguinas que tiene el json
-    {
-        $client = new Client('http://apiassets.upr.edu.cu');
-        $request = $client->get($tabla);
-        $response = $request->send();
-        $data = $response->json();        
-        $count= $data['hydra:totalItems'];
-        $total = $data['hydra:itemsPerPage'];
-        if ($count < $total ) {
-           return 0;
-        }
-        else
-        {        
-            $count= $data['hydra:lastPage'];
+    public function GetCantPageFilter($count)//obtener la cantidad de paguinas que tiene el json
+    {            
+           
             $a=preg_split("/[\s,=]+/", $count);        
-            return $a[2];    
-            //echo $count;
-           //print_r($a);
-        }
+            if(count($a)>2)
+                return $a[2];    
+            else
+                return 0;
+        
+    }
+
+    public function GetCantMultiFilter($count)
+    {
+        
+        $a=preg_split("/[\s,=,%]+/", $count);        
+        if(count($a)>3)
+            return $a[3];    
+        else
+            return 0;
+        
+
+        
     }
 
     
---
+++ web/bootstrap/js/custom_contable.js
@@ -0,0 +1,15 @@
+/*$(document).ready(function()
+	{
+ 		$("#mostrarmodal").modal("show");
+});
+ 
+function BuscarEmpleado(idEmpleado)   
+{
+    $('.titulo').html(idEmpleado);
+    $("#mostrarmodal").modal("show");
+}
+
+function BuscarAreaResponsable(idArearesp)
+{
+    alert(idArearesp);
+}
