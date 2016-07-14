1e0d569f31080e9b9ca2e63bfc04b784460192bb
Frank J Vigil Vega <frank.vigil@upr.edu.cu>
2016-07-14 13:05:59 -0400

    visual en cambios11

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),            
             new AppBundle\AppBundle(),            
             new Contabilidad\activosBundle\activosBundle(),
+            //new IMAG\LdapBundle\IMAGLdapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
