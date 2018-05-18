abfac932dfdc1274b86db7b037fc886555f97e02
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-18 15:07:21 -0400

    arreglando deploy-git

--- tools/composer.sh
+++ tools/composer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ENV=${1:-production}
+ENV=${1:-master}
 echo Deploying $ENV
-ssh -i /root/.ssh/id_rsa root@10.2.1.205 'cd /home/Contable php composer.phar update --prefer-dist -o'
+ssh -i /root/.ssh/id_rsa root@10.2.24.193 'cd /home/Contable/master && php ../../composer.phar update --prefer-dist -o'
 
--- tools/deploy-git.sh
+++ tools/deploy-git.sh
@@ -2,5 +2,5 @@
 
 ENV=${1:-master}
 echo Deploying $ENV
-ssh -i /root/.ssh/id_rsa root@10.2.1.205 'r10k deploy environment -p -v -c /etc/r10k.yaml'
+ssh -i /root/.ssh/id_rsa root@10.2.24.193 'r10k deploy environment -p -v -c /etc/r10k.yaml'
 
