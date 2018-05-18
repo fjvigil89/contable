5e040b66e8331c407d8c71751099ed4227923bc5
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-18 09:35:58 -0400

    arreglando deploy-git

--- tools/deploy-git.sh
+++ tools/deploy-git.sh
@@ -2,4 +2,5 @@
 
 ENV=${1:-production}
 echo Deploying $ENV
-ssh -i /root/.ssh/id_rsa root@10.2.1.205 'cd /root/ | /usr/bin/r10k puppetfile install'
+ssh -i /root/.ssh/id_rsa root@10.2.1.205 'r10k deploy environment -p -v -c /etc/r10k.yaml'
+
